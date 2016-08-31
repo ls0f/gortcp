1d98f2a3c4f7ad6d38bcce7055d19619012ec16b
lovedboy <admin@lovedboy.com>
2016-08-31 23:45:16 +0800

    debug (#6)
    * debug
    
    * 缩进

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-  
+set -e
+
+if [ ! -z "$TRAVIS_TAG" ];then
+	echo "the tag is $TRAVIS_TAG, will deploy...."
+else
+	echo "will not deploy..."
+	exit 0
+fi
 cd ./cmd/server/
 gox -output "../../dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
 
